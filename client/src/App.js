7c336a2b544f3a3ff804469d695117a0cc2bdaef
Tom Williams <tom.williams0593@outlook.com>
2021-03-05 02:04:05 +0000

    First draft design, added title, inputs and button for movie names and reviews.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Start Designing!</h1>
+      <h1>CRUD Application</h1>
+      <div className="form">
+        <label>Movie Name:</label>
+        <input type="text" name="MovieName" />
+        <label>Movie Review:</label>
+        <input type="text" name="MovieReview" />
+
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
