eb02e4a3d46457601de314d015985f2f9dfbc88f
Tom Williams <tom.williams0593@outlook.com>
2021-03-10 17:06:31 +0000

    Created a connection to the backend using axios.

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,42 @@
+import React, { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [movieName, setMovieName] = useState('');
+  const [movieReview, setMovieReview] = useState('');
+
+  const submitReview = () => {
+    Axios.post('http://localhost:5000/api/insert/', {
+      movieName: movieName,
+      movieReview: movieReview,
+    }).then(() => {
+      alert('Successful insert...');
+    });
+  };
+
   return (
     <div className="App">
       <h1>CRUD Application</h1>
       <div className="form">
         <label>Movie Name:</label>
-        <input type="text" name="MovieName" />
+        <input
+          type="text"
+          name="MovieName"
+          onChange={(e) => {
+            setMovieName(e.target.value);
+          }}
+        />
         <label>Movie Review:</label>
-        <input type="text" name="MovieReview" />
+        <input
+          type="text"
+          name="MovieReview"
+          onChange={(e) => {
+            setMovieReview(e.target.value);
+          }}
+        />
 
-        <button>Submit</button>
+        <button onClick={submitReview}>Submit</button>
       </div>
     </div>
   );
